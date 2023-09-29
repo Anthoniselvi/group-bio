20e64686bf0dd2a12c10c4a2e883f783af73bae5
Anthoniselvi <anthoniselvi33@gmail.com>
2023-09-29 15:08:35 +0530

    card alphabeticlly aligned & searching done by abc..

--- components/Dashboard/ProfileCard.js
+++ components/Dashboard/ProfileCard.js
@@ -19,12 +19,13 @@ import Image from "next/image";
 export default function ProfileCard() {
   const theme = useTheme();
   const router = useRouter();
-  const [data, setData] = useState([]);
+  const [profilesList, setProfilesList] = useState([]);
+  const [selectedLetter, setSelectedLetter] = useState(null);
 
   const navigateToSingleProfile = (item) => {
     router.push({
       pathname: "/singleprofile",
-      query: { id: item.profileId }, // Pass the clicked item's ID as a query parameter
+      query: { id: item.profileId },
     });
   };
 
@@ -32,143 +33,189 @@ export default function ProfileCard() {
     axios
       .get(`${process.env.NEXT_PUBLIC_BASE_URL}/profile/all`)
       .then((response) => {
-        setData(response.data);
-        console.log("Data:" + JSON.stringify(response.data));
+        const sortedProfiles = response.data.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+        setProfilesList(sortedProfiles);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
 
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  const filterProfilesByLetter = (letter) => {
+    const filtered = profilesList.filter(
+      (item) => item.name.charAt(0).toLowerCase() === letter
+    );
+    return filtered;
+  };
+
+  const handleLetterClick = (letter) => {
+    if (selectedLetter === letter) {
+      setSelectedLetter(null);
+      router.push({
+        pathname: "/filteredprofile",
+        query: { searchResult: JSON.stringify(profilesList), searchQuery: "" },
+      });
+    } else {
+      setSelectedLetter(letter);
+      const filteredData = filterProfilesByLetter(letter);
+      router.push({
+        pathname: "/filteredprofile",
+        query: {
+          searchResult: JSON.stringify(filteredData),
+          searchQuery: letter,
+        },
+      });
+    }
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "1rem",
-        cursor: "pointer",
-      }}
-    >
-      {data.map((item) => (
-        <Card
-          sx={{
-            display: "flex",
-            padding: "1em",
-            paddingTop: 0,
-            paddingBottom: 0,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          key={item.profileId}
-          onClick={() => navigateToSingleProfile(item)}
-        >
-          {/* <CardMedia
-            component="img"
+    <div style={{ display: "flex", gap: "1rem" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          position: "relative",
+          gap: "1rem",
+        }}
+      >
+        {profilesList.map((item) => (
+          <Card
             sx={{
-              width: 70,
-              height: 70,
-              borderRadius: "50%",
+              display: "flex",
+              padding: "0 1em",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              boxShadow: "0px 1px 5px 1px #121212",
             }}
-            image={Photo}
-            alt="Profile Image"
-          /> */}
-          <CardMedia
-            component="div"
-            sx={{
-              width: 80,
-              height: 80,
-              borderRadius: "50%",
-              overflow: "hidden",
-            }}
+            key={item.profileId}
+            onClick={() => navigateToSingleProfile(item)}
           >
-            {/* {console.log("image: " + item.image)} */}
-            <img
-              src={item.image}
-              style={{ width: "100%", height: "100%" }}
-              alt="image"
-            />
-            {/* <Image
+            <CardMedia
+              component="div"
+              sx={{
+                width: 80,
+                height: 80,
+                borderRadius: "50%",
+                overflow: "hidden",
+              }}
+            >
+              <img
+                src={item.image}
+                style={{ width: "100%", height: "100%" }}
+                alt="image"
+              />
+              {/* <Image
               src={`/${item.image}`}
               alt="Profile Image"
               width={70}
               height={70}
               objectFit="cover"
             /> */}
-          </CardMedia>
+            </CardMedia>
 
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "top",
-              width: "100%",
-            }}
-          >
-            <CardContent
+            <Box
               sx={{
-                // flex: "1 0 auto",
                 display: "flex",
                 flexDirection: "column",
-                gap: "0.5em",
-                padding: 0,
-                paddingLeft: "1em",
-                paddingTop: "1em",
+                alignItems: "top",
+                width: "100%",
               }}
             >
-              <Typography
-                component="div"
-                // variant="h5"
+              <CardContent
                 sx={{
-                  fontFamily: "Sans-serif",
-                  fontSize: "17px",
-                  fontWeight: 600,
+                  // flex: "1 0 auto",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "0.5em",
+                  padding: 0,
+                  paddingLeft: "1em",
+                  paddingTop: "1em",
                 }}
               >
-                {item.name}
-              </Typography>
-              <Typography
-                component="div"
-                // variant="h5"
-                sx={{
-                  fontFamily: "Sans-serif",
-                  fontSize: "15px",
-                }}
-              >
-                {item.course}, {item.year}
-              </Typography>
+                <Typography
+                  component="div"
+                  // variant="h5"
+                  sx={{
+                    fontFamily: "Sans-serif",
+                    fontSize: "17px",
+                    fontWeight: 600,
+                  }}
+                >
+                  {item.name}
+                </Typography>
+                <Typography
+                  component="div"
+                  // variant="h5"
+                  sx={{
+                    fontFamily: "Sans-serif",
+                    fontSize: "15px",
+                  }}
+                >
+                  {item.course}, {item.year}
+                </Typography>
 
-              <Typography
-                sx={{
-                  fontFamily: "Sans-serif",
-                  fontSize: "14px",
-                  color: "#999999",
-                }}
-                // variant="subtitle1"
-                // color="text.secondary"
-                component="div"
-              >
-                {item.designation}, {item.company}
-                <br />
-                {item.location}
-              </Typography>
+                <Typography
+                  sx={{
+                    fontFamily: "Sans-serif",
+                    fontSize: "14px",
+                    color: "#999999",
+                  }}
+                  // variant="subtitle1"
+                  // color="text.secondary"
+                  component="div"
+                >
+                  {item.designation}, {item.company}
+                  <br />
+                  {item.location}
+                </Typography>
 
-              <Typography
-                sx={{
-                  fontFamily: "Sans-serif",
-                  fontSize: "14px",
-                  color: "#000000",
-                }}
-                // variant="subtitle1"
-                // color="text.secondary"
-                component="div"
-              >
-                Services Offered: <br />
-                {item.offers}
-              </Typography>
-            </CardContent>
-          </Box>
-        </Card>
-      ))}
+                <Typography
+                  sx={{
+                    fontFamily: "Sans-serif",
+                    fontSize: "14px",
+                    color: "#000000",
+                  }}
+                  // variant="subtitle1"
+                  // color="text.secondary"
+                  component="div"
+                >
+                  Services Offered: <br />
+                  {item.offers}
+                </Typography>
+              </CardContent>
+            </Box>
+          </Card>
+        ))}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          position: "absolute",
+          zIndex: 4,
+          padding: "0px 5px",
+          right: 5,
+          backgroundColor: "#f8edeb",
+          borderRadius: "10px",
+        }}
+      >
+        {alphabet.map((letter) => (
+          <p
+            key={letter}
+            onClick={() => handleLetterClick(letter)}
+            // ...rest of the button styling...
+          >
+            {letter.toUpperCase()}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
