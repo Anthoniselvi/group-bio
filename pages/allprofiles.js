e02e7e053d1f20784624b4b89c5af0dbe219b59c
Anthoniselvi <anthoniselvi33@gmail.com>
2023-09-30 16:34:39 +0530

    add button added on home page

--- pages/allprofiles.js
+++ pages/allprofiles.js
@@ -9,12 +9,41 @@ import TextField from "@mui/material/TextField"; // Import TextField from Materi
 import axios from "axios";
 import Footer from "@/components/Footer/Footer";
 import ProfileCard from "@/components/Dashboard/ProfileCard";
+import { useRouter } from "next/router";
 const steps = ["Personal Information", "Company Information"];
 
 export default function AllProfiles() {
+  const router = useRouter();
+  const navigateToCreateProfile = () => {
+    router.push({
+      pathname: "/form",
+    });
+  };
   return (
     <div style={{ padding: "1rem", paddingTop: "6rem" }}>
-      <h2 style={{ paddingBottom: "1rem" }}>Members</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingBottom: "1rem",
+        }}
+      >
+        <h2>Members</h2>
+        <button
+          style={{
+            padding: "10px 20px",
+            backgroundColor: "#00b4d8",
+            color: "#fff",
+            borderRadius: "20px",
+            border: "none",
+            cursor: "pointer",
+          }}
+          onClick={navigateToCreateProfile}
+        >
+          + Add
+        </button>
+      </div>
       <ProfileCard />
     </div>
   );
