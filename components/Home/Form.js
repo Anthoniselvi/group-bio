efc8c82c074f8b58a7a9778c9ccebad72038934a
Anthoniselvi <anthoniselvi33@gmail.com>
2023-10-02 18:35:37 +0530

    error removed once filled input

--- components/Home/Form.js
+++ components/Home/Form.js
@@ -141,11 +141,8 @@ export default function Form() {
 
   const handleFieldChange = (event, fieldLabel) => {
     const newValue = event.target.value;
-    setInputFieldValues((prevValues) => ({
-      ...prevValues,
-      [fieldLabel]: newValue,
-    }));
 
+    // Validate the field and store the error in the fieldErrors state
     let validationFunction = null;
 
     switch (fieldLabel) {
@@ -188,38 +185,104 @@ export default function Form() {
       switch (fieldLabel) {
         case "name":
           setIsNameError(!!error.name);
+          if (error.name) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "course":
           setIsCourseError(!!error.course);
+          if (error.course) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "year":
           setIsYearError(!!error.year);
+          if (error.year) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "location":
           setIsLocationError(!!error.location);
+          if (error.location) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "company":
           setIsCompanyError(!!error.company);
+          if (error.company) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "designation":
           setIsDesignationError(!!error.designation);
+          if (error.designation) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "industry":
           setIsIndustryError(!!error.industry);
+          if (error.industry) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "offers":
           setIsOffersError(!!error.offers);
+          if (error.offers) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "linkedin":
           setIsLinkedinError(!!error.linkedin);
+          if (error.linkedin) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         case "website":
           setIsWebsiteError(!!error.website);
+          if (error.website) {
+            setFieldErrors((prevErrors) => ({
+              ...prevErrors,
+              [fieldLabel]: "",
+            }));
+          }
           break;
         default:
           break;
       }
     }
+
+    // Update the field value in the inputFieldValues state
+    setInputFieldValues((prevValues) => ({
+      ...prevValues,
+      [fieldLabel]: newValue,
+    }));
 
     const currentStepFilledFields = filledFields[activeStep];
     if (newValue) {
